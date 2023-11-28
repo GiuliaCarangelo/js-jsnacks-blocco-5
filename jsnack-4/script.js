3a5c8f1b78d48470626bfe566d4e14f37e1b901b
GiuliaCarangelo <tnhkagura@gmail.com>
2023-11-28 12:25:18 +0100

    add arrays

---
+++ jsnack-4/script.js
@@ -0,0 +1,13 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi)
+// prendendo alternativamente gli elementi da uno e dall'altro
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+
+
+function(firstArray, secondArray){
+
+    // function sum
+    let firstArray = [a, b, c, d, e];
+    let secondArray = [1, 2, 3, 4, 5];
+    console.log(sum)
+}
