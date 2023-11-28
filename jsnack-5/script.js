d837e3488f98185790db049e36281b0217b7ab60
GiuliaCarangelo <tnhkagura@gmail.com>
2023-11-28 12:37:18 +0100

    add objects' array

---
+++ jsnack-5/script.js
@@ -0,0 +1,23 @@
+// Creare un array di oggetti di squadre di volley.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+// Generare numeri random al posto delle nelle proprietà:
+// punti fatti e falli subiti
+
+const volleyTeamList = [
+    {
+        "name": "TeamOne",
+        "score": 31,
+        "lostPoints": 10
+    },
+    {
+        "name": "TeamTwo",
+        "score": 12,
+        "lostPoints": 18
+    },
+    {
+        "name": "TeamThree",
+        "score": 2,
+        "lostPoints": 1
+    }
+]
+
